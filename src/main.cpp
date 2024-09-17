ce3d412fed817ce70f47f41aa52b0c5fa2daf83c
yudai <yudai.yy0804@gmail.com>
2024-09-17 11:07:06 +0900

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <mysql_connection.h>
-#include <mysql_driver.h>
-#include <mysql_error.h>
-
 #include <chrono>
 #include <deque>
 #include <iostream>
@@ -16,79 +8,18 @@
 #include <utility>
 #include <vector>
 
+#include "wikipedia.hpp"
 // for HOST, USER, PASSWORD
 #include "private_config.hpp"
 
-class MySQLController {
-private:
-  std::string host;
-  std::string user;
-  std::string password;
-  sql::mysql::MySQL_Driver *driver;
-  std::shared_ptr<sql::Connection> con;
-  std::shared_ptr<sql::Statement> stmt;
-  std::map<std::string, int> mp;
-
-public:
-  MySQLController(std::string _host, std::string _user, std::string _password)
-      : host(_host), user(_user), password(_password) {}
-  void init() {
-    std::shared_ptr<sql::ResultSet> res;
-    driver = sql::mysql::get_mysql_driver_instance();
-    con =
-        std::shared_ptr<sql::Connection>(driver->connect(host, user, password));
-    stmt = std::shared_ptr<sql::Statement>(con->createStatement());
-    stmt->execute("USE jawikipedia");
-
-    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
-        "SELECT page_title,page_id FROM page WHERE page_namespace=0"));
-    while (res->next()) {
-      mp[res->getString("page_title")] = res->getInt("page_id");
-    }
-  }
-  int page_title_to_page_id(std::string page_title) {
-    std::shared_ptr<sql::ResultSet> res;
-    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
-        "SELECT page_id FROM page WHERE page_namespace=0 AND page_title='" +
-        page_title + "'"));
-    int page_id = -1;
-    if (res->next()) {
-      page_id = res->getInt("page_id");
-    }
-    return page_id;
-  }
-  std::string page_id_to_page_title(int page_id) {
-    std::shared_ptr<sql::ResultSet> res;
-    std::string page_title;
-    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
-        "SELECT page_title FROM page WHERE page_namespace=0 AND page_id=" +
-        std::to_string(page_id)));
-    if (res->next()) {
-      page_title = res->getString("page_title");
-    }
-    return page_title;
-  }
-  void search(int page_id, std::vector<int> &to) {
-    std::shared_ptr<sql::ResultSet> res;
-    // リンクの個数を数える。リンク先のページが存在していないもの(wikipedia上の赤文字のリンク)はカウントしない
-    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
-        "SELECT lt_title FROM linktarget WHERE lt_namespace=0 AND lt_id IN (SELECT pl_target_id FROM pagelinks WHERE pl_from=" +
-        std::to_string(page_id) + ")"));
-    while (res->next()) {
-      auto page_id = mp[res->getString("lt_title")];
-      to.push_back(page_id);
-    }
-  }
-};
-
-MySQLController mysql(HOST, USER, PASSWORD);
+Wikipedia wiki(HOST, USER, PASSWORD);
 std::map<int, std::pair<std::vector<int>, int>> graph;
 int MAX_DEPTH = 3;
 int inf = 1e9;
 
 void search(std::string start, std::string goal) {
-  int start_page_id = mysql.page_title_to_page_id(start);
-  int goal_page_id = mysql.page_title_to_page_id(goal);
+  int start_page_id = wiki.page_title_to_page_id(start);
+  int goal_page_id = wiki.page_title_to_page_id(goal);
   if (start_page_id == -1 || goal_page_id == -1) {
     std::cout << "error" << std::endl;
     return;
@@ -102,7 +33,7 @@ void search(std::string start, std::string goal) {
     q.pop();
     if (visit.count(page_id)) continue;
     visit.insert(page_id);
-    mysql.search(page_id, graph[page_id].first);
+    wiki.search(page_id, graph[page_id].first);
     graph[page_id].second = cost;
     for (auto _ : graph[page_id].first) {
       if (visit.count(_)) continue;
@@ -139,7 +70,7 @@ void search(std::string start, std::string goal) {
   dfs(dfs, goal_page_id, graph[goal_page_id].second + 1);
   std::cout << "answer" << std::endl;
   for (int i = 0; i < ans.size(); i++)
-    std::cout << i << ":" << mysql.page_id_to_page_title(ans[i]) << std::endl;
+    std::cout << i << ":" << wiki.page_id_to_page_title(ans[i]) << std::endl;
 }
 
 int main(int argc, char **argv) {
@@ -149,8 +80,8 @@ int main(int argc, char **argv) {
   }
   std::string target = argv[1];
   std::string goal = argv[2];
-  mysql.init();
-  auto start_page_id = mysql.page_title_to_page_id(target);
+  wiki.init();
+  auto start_page_id = wiki.page_title_to_page_id(target);
   // 開始時刻を記録
   auto start = std::chrono::high_resolution_clock::now();
 
---
+++ src/wikipedia.hpp
@@ -0,0 +1,92 @@
+/**
+ * @file wikipedia.hpp
+ * @author Yudai Yamaguchi (yudai.yy0804@gmail.com)
+ * @brief wikipediaのSQLを制御するクラス
+ * @version 0.1
+ * @date 2024-09-17
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+#include <mysql_error.h>
+
+#include <chrono>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <utility>
+#include <vector>
+
+class Wikipedia {
+private:
+  std::string host;
+  std::string user;
+  std::string password;
+  sql::mysql::MySQL_Driver *driver;
+  std::shared_ptr<sql::Connection> con;
+  std::shared_ptr<sql::Statement> stmt;
+  std::map<std::string, int> mp;
+
+public:
+  Wikipedia(std::string _host, std::string _user, std::string _password)
+      : host(_host), user(_user), password(_password) {}
+  void init() {
+    std::shared_ptr<sql::ResultSet> res;
+    driver = sql::mysql::get_mysql_driver_instance();
+    con =
+        std::shared_ptr<sql::Connection>(driver->connect(host, user, password));
+    stmt = std::shared_ptr<sql::Statement>(con->createStatement());
+    stmt->execute("USE jawikipedia");
+
+    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
+        "SELECT page_title,page_id FROM page WHERE page_namespace=0"));
+    while (res->next()) {
+      mp[res->getString("page_title")] = res->getInt("page_id");
+    }
+  }
+  int page_title_to_page_id(std::string page_title) {
+    std::shared_ptr<sql::ResultSet> res;
+    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
+        "SELECT page_id FROM page WHERE page_namespace=0 AND page_title='" +
+        page_title + "'"));
+    int page_id = -1;
+    if (res->next()) {
+      page_id = res->getInt("page_id");
+    }
+    return page_id;
+  }
+  std::string page_id_to_page_title(int page_id) {
+    std::shared_ptr<sql::ResultSet> res;
+    std::string page_title;
+    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
+        "SELECT page_title FROM page WHERE page_namespace=0 AND page_id=" +
+        std::to_string(page_id)));
+    if (res->next()) {
+      page_title = res->getString("page_title");
+    }
+    return page_title;
+  }
+  void search(int page_id, std::vector<int> &to) {
+    std::shared_ptr<sql::ResultSet> res;
+    // リンクの個数を数える。リンク先のページが存在していないもの(wikipedia上の赤文字のリンク)はカウントしない
+    res = std::shared_ptr<sql::ResultSet>(stmt->executeQuery(
+        "SELECT lt_title FROM linktarget WHERE lt_namespace=0 AND lt_id IN (SELECT pl_target_id FROM pagelinks WHERE pl_from=" +
+        std::to_string(page_id) + ")"));
+    while (res->next()) {
+      auto page_id = mp[res->getString("lt_title")];
+      to.push_back(page_id);
+    }
+  }
+};
